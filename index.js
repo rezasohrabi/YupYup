b3d11e8b9b857f604409752bb84c7500bec5f27b
rezasohrabi <rezasohrabi1995@yahoo.com>
2023-02-19 18:32:28 +0330

    refactor: move server to express

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const http = require('http');
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.write('Hello World');
-        res.end();
-    }
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-server.listen(3000);
-
-console.log('Listening on port 3000...')
+app.listen(3000, console.log("listening on port 3000..."));
