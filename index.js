7d171ca840989e3cff7bba27fa1902ee161e6069
rezasohrabi <rezasohrabi1995@yahoo.com>
2023-02-21 22:49:50 +0330

    feat: add users route

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require("express");
 
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(users);
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 const port = process.env.PORT || 3000;
