83409230abc394cd8d424f1674953fa17e3d0855
rezasohrabi <rezasohrabi1995@yahoo.com>
2023-02-21 21:15:06 +0330

    feat: read port from env

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, console.log("listening on port 3000..."));
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`listening on port ${port}...`));
