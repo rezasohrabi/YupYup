c9619ead25951629647de3899b5de72fc4c6bc27
rezasohrabi <rezasohrabi1995@yahoo.com>
2023-08-10 20:54:06 +0330

    feat: add users crud to mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Joi = require("joi");
+const UserModel = require("./../models/User");
 
 const router = express.Router();
 
@@ -18,32 +19,47 @@ const users = [
   },
 ];
 
-router.get("/", (req, res) => {
-  res.send(users);
+router.get("/", async (req, res) => {
+  const { page, count } = req.query;
+  console.log(req.query);
+  const total = await UserModel.find().count();
+  const users = await UserModel.find()
+    .skip((page - 1) * count)
+    .limit(count);
+  res.send({
+    data: users,
+    meta: {
+      page: page,
+      total,
+    },
+  });
 });
 
-router.get("/:id", (req, res) => {
-  const user = users.find((user) => user.id === parseInt(req.params.id));
+router.get("/:id", async (req, res) => {
+  const user = await UserModel.findOne({ _id: req.params.id });
   if (!user) {
     return res.status(404).send("user not found");
   }
   res.send(user);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateUser(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  const user = {
-    id: users.length + 1,
+  const user = new UserModel({
     username: req.body.username,
-    password: req.body.password,
+    email: req.body.email,
+    job: req.body.job,
     age: req.body.age,
-  };
-  users.push(user);
-  res.send(user);
+    hobbies: req.body.hobbies,
+  });
+
+  const result = await user.save();
+
+  res.send(result);
 });
 
 router.put("/:id", (req, res) => {
@@ -63,22 +79,22 @@ router.put("/:id", (req, res) => {
   res.send(user);
 });
 
-router.delete("/:id", (req, res) => {
-  const user = users.find((user) => user.id === parseInt(req.params.id));
+router.delete("/:id", async (req, res) => {
+  const user = await UserModel.findByIdAndRemove({ _id: req.params.id });
   if (!user) {
     return res.status(404).send("user not found");
   }
 
-  const index = users.indexOf(user);
-  users.splice(index, 1);
   res.send(user);
 });
 
 function validateUser(body) {
   const schema = Joi.object({
-    username: Joi.string().min(8).required(),
-    password: Joi.string(),
+    username: Joi.string().alphanum().min(3).required(),
     age: Joi.number().min(18).max(70).required(),
+    email: Joi.string().email().required(),
+    hobbies: Joi.array(),
+    job: Joi.string(),
   });
   return schema.validate(body);
 }
